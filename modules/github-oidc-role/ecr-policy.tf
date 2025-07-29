e62f6410018d5078bceddab97193700b3a710f85
Maciej Wilczyński <maciej.wilczynski@letsgodevops.pl>
2025-07-29 17:04:41 +0200

    Add GitHub OIDC configuration modules

---
+++ modules/github-oidc-role/ecr-policy.tf
@@ -0,0 +1,49 @@
+data "aws_iam_policy_document" "github_ecr_push_policy" {
+  statement {
+    actions = [
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:BatchGetImage",
+      "ecr:CompleteLayerUpload",
+      "ecr:DescribeImages",
+      "ecr:DescribeRepositories",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:GetRepositoryPolicy",
+      "ecr:InitiateLayerUpload",
+      "ecr:ListImages",
+      "ecr:PutImage",
+      "ecr:UploadLayerPart",
+    ]
+
+    resources = var.ecr_push_policy.repository_arns
+  }
+
+  statement {
+    effect    = "Allow"
+    resources = ["*"]
+    actions   = ["ecr:GetAuthorizationToken"]
+  }
+
+  statement {
+    actions   = ["kms:GenerateDataKey"]
+    resources = ["arn:aws:kms:*:${data.aws_caller_identity.current.account_id}:key/*"]
+
+    condition {
+      test     = "ForAnyValue:StringEquals"
+      variable = "kms:ResourceAliases"
+      values   = var.ecr_push_policy.kms_key_aliases
+    }
+  }
+}
+
+resource "aws_iam_policy" "github_ecr_push_policy" {
+  count       = var.ecr_push_policy.enabled ? 1 : 0
+  name        = "github-ecr-push-policy"
+  description = "Policy for GitHub Actions to push to ECR"
+  policy      = data.aws_iam_policy_document.github_ecr_push_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "github_ecr_push_policy" {
+  count      = var.ecr_push_policy.enabled ? 1 : 0
+  role       = aws_iam_role.this.name
+  policy_arn = aws_iam_policy.github_ecr_push_policy[0].arn
+}
